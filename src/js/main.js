ea873ab22e7ab0cd4c94fd384349e160c2ba6c34
nizomiddinqarshiyev <qarshiyevnizomiddin75@gmail.com>
2025-09-30 19:36:58 +0500

    some changes

--- src/js/main.js
+++ src/js/main.js
@@ -706,23 +706,5 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-$(document).ready(function(){
-    const slider = $("#slider").owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:5
-            }
-        }
-    });
-});
 
 
